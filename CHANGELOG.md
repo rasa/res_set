9dd045294db422942136c41ca45b766296f3506a
Ross Smith II <ross@smithii.com>
2015-05-20 20:58:14 -0700

    Update Makefile

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.10 (unreleased)
+
+  * Update Makefile
+  
 ## 1.9 (28-Mar-2015)
 
   * Fix segfault due to linker issue (#1)
--- version.h
+++ version.h
@@ -3,7 +3,7 @@
 #define VER_INTERNAL_NAME 		"res_set"
 #define	VER_FILE_DESCRIPTION	"Display or change the monitor resolution, color depth, or refresh rate"
 #define VER_MAJOR				1
-#define VER_MINOR				9
-#define VER_STRING2				"1.9"
+#define VER_MINOR				10
+#define VER_STRING2				"1.10"
 
 #include "ver_defaults.h"
